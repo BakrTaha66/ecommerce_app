1e4c9280d61ea51b6ef8f14d233ce0ac96136115
BakrTaha66 <bakrtaha505@gmail.com>
2022-12-17 04:12:17 +0200

    model

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/model/product_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Dio dio = Dio();
+  String url = 'https://fakestoreapi.com/products';
+  ProductsModel? productsModel;
+
+  Future<ProductsModel> getData() async {
+    Response response = await dio.get(url);
+
+    productsModel = ProductsModel.fromJson(response.data);
+    return productsModel!;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
